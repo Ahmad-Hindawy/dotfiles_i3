4fb118f2eba8d130ed12dd11449d7f88c021466e
Ahmad-Hindawy <hindawyvb@gmail.com>
2021-03-03 23:51:08 +0200

    adding sound volume effect
    adding gui app

--- scripts/exit.sh
+++ scripts/exit.sh
@@ -4,12 +4,12 @@ choice=$(echo "Shutdown\nReboot\nExit" | dmenu -i -p "Choose action: ")
 #echo $choice | dmenu
 if [ $choice = "Shutdown" ];
 then
-sudo -A shutdown -h now
+#systemctl poweroff
+shutdown -h now
 elif [ $choice = "Reboot" ];
 then
-sudo -A shutdown -r now
+shutdown -r now
 elif [ $choice = "Exit" ];
 then
-#i3-msg exit
-echo hi
+i3-msg exit
 fi
